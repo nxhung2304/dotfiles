e623d8579ad16233c2529d1e39b3dd9c853e7a73
Nguyen Xuan Hung <mongtamquoc2015@gmail.com>
2025-02-20 22:05:09 +0700

    nvim: use cmp instead of blink.nvim

--- .config/nvim/lua/user/plugins/cmp.lua
+++ .config/nvim/lua/user/plugins/cmp.lua
@@ -1,39 +1,114 @@
 return {
-	"saghen/blink.cmp",
-	event = "LspAttach",
-	version = "*",
-	opts = {
-		keymap = { preset = "super-tab" },
-
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lua",
+			"saadparwaiz1/cmp_luasnip",
 		},
+		opts = function()
+			local luasnip = require("luasnip")
+			local cmp = require("cmp")
+			require("luasnip/loaders/from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/lua/user/snippets" })
 
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
-		completion = {
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 300,
-				update_delay_ms = 50,
-				treesitter_highlighting = true,
+			local col = vim.fn.col(".") - 1
+			local check_backspace = col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+			return {
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				mapping = {
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+					["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+					["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+					["<C-e>"] = cmp.mapping({
+						i = cmp.mapping.abort(),
+						c = cmp.mapping.close(),
+					}),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expandable() then
+							luasnip.expand()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif check_backspace then
+							fallback()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+						c = cmp.config.disable,
+					}),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+				},
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						local kind =
+							require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+						local strings = vim.split(kind.kind, "%s", { trimempty = true })
+						kind.kind = " " .. (strings[1] or "") .. " "
+						kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+						return kind
+					end,
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip", priority = 8 },
+					{ name = "buffer" },
+					{ name = "path" },
+				},
+				confirm_opts = {
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = false,
+				},
 				window = {
-					min_width = 10,
-					max_width = 80,
-					max_height = 20,
-					border = "padded",
-					winblend = 0,
-					winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
-					scrollbar = true,
-					direction_priority = {
-						menu_north = { "e", "w", "n", "s" },
-						menu_south = { "e", "w", "s", "n" },
+					documentation = {
+						border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					},
+					completion = {
+						col_offset = -3,
+						side_padding = 0,
+						border = "rounded",
+						winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
 					},
 				},
-			},
-		},
+				experimental = {
+					ghost_text = false,
+					native_menu = false,
+				},
+			}
+		end,
 	},
-	opts_extend = { "sources.default" },
 }
