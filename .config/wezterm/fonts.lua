0b28a50157b9e861012737b7bb873d29600bb496
Hung <nguyenxuan.hung@rights-s.jp>
2025-06-21 18:05:07 +0900

    wezterm: change font

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -2,7 +2,7 @@ local M = {}
 local wezterm = require("wezterm")
 
 function M.setup(config)
-	config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
+	config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 	config.font_size = 13
 	config.line_height = 1.4
 end
