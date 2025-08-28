1652d1b1d9f795fb879ee093ab5475363f3d65f5
Hung <mongtamquoc2015@gmail.com>
2025-08-28 09:49:01 +0700

    nvim: add catpuccin theme

--- .config/nvim/lua/user/plugins/flutter/init.lua
+++ .config/nvim/lua/user/plugins/flutter/init.lua
@@ -48,12 +48,9 @@ return {
 				enabled = true,
 				filter = nil,
 				notify_errors = true,
-				-- open_cmd = "30vnew",
-				open_cmd = "tabnew | term",
-				focus_on_open = true,
+				open_cmd = "30vnew",
+				focus_on_open = false,
 			},
-
-			default_run_args = { flutter = "--no-sound-null-safety" },
 
 			dev_tools = {
 				autostart = false,
@@ -94,22 +91,12 @@ return {
 
 		require("flutter-tools").setup_project({
 			{
-				name = "iPhone 16 - Dev", -- an arbitrary name that you provide so you can recognise this config
-				-- flavor = "DevFlavor", -- your flavour
-				-- target = "lib/main.dart", -- your target
-				device = "00008140-000C6D961447001C", -- the device ID, which you can get by running `flutter devices`
-				-- dart_define = {
-				-- 	API_URL = "https://dev.example.com/api",
-				-- 	IS_DEV = true,
-				-- },
-				-- pre_run_callback = nil, -- optional callback to run before the configuration
-				-- -- exposes a table containing name, target, flavor and device in the arguments
-				-- dart_define_from_file = "config.json", -- the path to a JSON configuration file
+				name = "iPhone 16 - Dev",
+				device = "00008140-000C6D961447001C",
 			},
 			{
 				name = "Web - Dev",
 				device = "chrome",
-				-- flavor = "WebApp",
 				web_port = "4000",
 				additional_args = { "--wasm" },
 			},
@@ -123,30 +110,28 @@ return {
 	keys = {
 		{
 			"<leader>Fs",
-			"FlutterRun",
+			"<cmd>FlutterRun<cr>",
 			desc = "Flutter Run",
 		},
 		{
 			"<leader>Fq",
-			function()
-				vim.cmd("FlutterQuit")
-			end,
+      "<cmd>FlutterQuit<cr>",
 			desc = "Stop Debug & Quit Flutter",
 		},
 
 		{
 			"<leader>Fr",
-			"FlutterReload",
+			"<cmd>FlutterReload<cr>",
 			desc = "Flutter Hot Reload (Debug)",
 		},
 		{
 			"<leader>FR",
-			"FlutterRestart",
+			"<cmr>FlutterRestart<cr>",
 			desc = "Flutter Hot Restart (Debug)",
 		},
 		{
 			"<leader>Fd",
-			"FlutterDevices",
+			"<cmd>FlutterDevices<cr>",
 			desc = "Select devices",
 		},
 
