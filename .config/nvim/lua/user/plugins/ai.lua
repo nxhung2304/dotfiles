160588863fe68d3e8b20715b63def6c740772e61
Hung <mongtamquoc2015@gmail.com>
2025-12-02 10:25:47 +0700

    nvim: add codecompanion.nvim plugin

--- .config/nvim/lua/user/plugins/ai.lua
+++ .config/nvim/lua/user/plugins/ai.lua
@@ -29,7 +29,7 @@ return {
 						open = "<M-CR>",
 					},
 					layout = {
-						position = "bottom", -- | top | left | right
+						position = "bottom",
 						ratio = 0.4,
 					},
 				},
@@ -45,6 +45,64 @@ return {
 					["."] = false,
 				},
 			})
+			vim.keymap.set("n", "<leader>ct", function()
+				require("copilot.suggestion").toggle_auto_trigger()
+				local is_auto = require("copilot.suggestion").is_auto_trigger_enabled()
+				vim.cmd.echo("Copilot auto-trigger: " .. (is_auto and "ON" or "OFF"))
+			end, { desc = "Toggle Copilot auto-trigger" })
 		end,
+	},
+	{
+		"olimorris/codecompanion.nvim",
+		tag = "v17.33.0",
+		opts = {
+			strategies = {
+				inline = {
+					enabled = true,
+					marks = {
+						last = "●",
+					},
+					format = {
+						inline = { pattern = "^%s*<<<\\s*(.-)\\s*>>>", group = "Statement" },
+					},
+				},
+			},
+			display = {
+				render = {
+					inline = {
+						layout = "float",
+					},
+				},
+			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"ravitemer/mcphub.nvim",
+			{
+				"OXY2DEV/markview.nvim",
+				lazy = false,
+				opts = {
+					preview = {
+						filetypes = { "markdown", "codecompanion" },
+						ignore_buftypes = {},
+					},
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>ac",
+				"<cmd>CodeCompanionChat Toggle<cr>",
+				mode = { "n", "v" },
+				desc = "Toggle CodeCompanion Chat",
+			},
+			{
+				"<leader>aa",
+				"<cmd>CodeCompanionActions<cr>",
+				mode = { "n", "v" },
+				desc = "CodeCompanion Actions Palette",
+			},
+			{ "<leader>av", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "Add visual selection to Chat" },
+		},
 	},
 }
